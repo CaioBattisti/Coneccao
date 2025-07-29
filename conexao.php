9ae8d0eb6e845914ca7e6810c4cc3e2ee3c9f007
CaioBattisti <cluizbattisti@gmail.com>
2025-07-29 19:54:38 -0300

    save7

--- conexao.php
+++ conexao.php
@@ -12,7 +12,7 @@
             try {
                 $conexao = new PDO($dsn, $usuario, $senha[
                 PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC;
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
             ]);
                 return $conexao;
             } catch (PDOException $e) {
