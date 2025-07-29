7702ed54b223b3012857d2f7fefbd20ebd8c37bf
CaioBattisti <cluizbattisti@gmail.com>
2025-07-29 19:33:22 -0300

    save1

---
+++ conexao.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <title>conexão</title>
+</head>
+<body>
+    <?php
+        function conectarBanco(){
+            $dsn = 'mysql:host=localhost;dbname=empresa;charset=utf8';
+            $usuario = 'root';
+            $senha = '';
+            try {
+                $conexao = new PDO($dsn, $usuario, $senha[;
+                PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC;
+            ]);
+                return $conexao;
+            } catch (PDOException $e) {
+                error_log("Erro de conctar aoa banco:".$e->get_Message());
+                // LOG SEM EXPOR ERRO AO USUARIO
+                die("Erro ao conectar ao Banco.");  
+        }
+    }
+    ?>
+</body>
+</html>
